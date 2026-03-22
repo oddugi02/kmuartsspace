72964d516f9b8161a5934e10672c9844b42d105a
김아영 <ahbbang@gim-ayeong-ui-MacBookPro.local>
2026-03-22 20:46:20 +0900

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+/** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: "export",
+  basePath: isGithubActions ? "/kmuartsspace" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default config;
